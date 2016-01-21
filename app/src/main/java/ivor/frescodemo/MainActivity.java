1f4b7b18b27119c89dde38b9036856985887f0b2
Ivor <845407259@qq.com>
2016-01-21 16:02:32 +0800

    update settings

--- app/src/main/java/ivor/frescodemo/MainActivity.java
+++ app/src/main/java/ivor/frescodemo/MainActivity.java
@@ -1,12 +1,19 @@
 package ivor.frescodemo;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
 import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.request.BasePostprocessor;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.facebook.imagepipeline.request.Postprocessor;
 
 public class MainActivity extends Activity {
 
@@ -14,9 +21,16 @@ public class MainActivity extends Activity {
     private SimpleDraweeView mPic2SDV;
     private SimpleDraweeView mPic3SDV;
     private SimpleDraweeView mPic4SDV;
+    private SimpleDraweeView mPic5SDV;
+    private SimpleDraweeView mPic6SDV;
+    private SimpleDraweeView mPic7SDV;
+    private SimpleDraweeView mPic8SDV;
+    private SimpleDraweeView mPic9SDV;
 
     private String mIcon = "http://g.hiphotos.baidu.com/image/pic/item/eac4b74543a9822681e4b42e8d82b9014a90eb96.jpg";
-    private String mGif= "http://imgsrc.baidu.com/forum/w%3D580/sign=792e812c564e9258a63486e6ac82d1d1/b912c8fcc3cec3fd8c0ece3dd688d43f87942741.jpg";
+    private String mGif  = "http://img.sc115.com/uploads/shows/140830/20140830549.jpg";
+
+    private PicHolding picHolding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +45,28 @@ public class MainActivity extends Activity {
         this.mPic2SDV = (SimpleDraweeView) findViewById(R.id.ivor_pic2_sdv);
         this.mPic3SDV = (SimpleDraweeView) findViewById(R.id.ivor_pic3_sdv);
         this.mPic4SDV = (SimpleDraweeView) findViewById(R.id.ivor_pic4_sdv);
+        this.mPic5SDV = (SimpleDraweeView) findViewById(R.id.ivor_pic5_sdv);
+        this.mPic6SDV = (SimpleDraweeView) findViewById(R.id.ivor_pic6_sdv);
+        this.mPic7SDV = (SimpleDraweeView) findViewById(R.id.ivor_pic7_sdv);
+        this.mPic8SDV = (SimpleDraweeView) findViewById(R.id.ivor_pic8_sdv);
+        this.mPic9SDV = (SimpleDraweeView) findViewById(R.id.ivor_pic9_sdv);
 
+        this.picHolding = new PicHolding();
+
+        // 普通图片处理
         Uri uri = Uri.parse(mIcon);
         this.mPic1SDV.setImageURI(uri);
         this.mPic2SDV.setImageURI(uri);
         this.mPic3SDV.setImageURI(uri);
 
-        DraweeController controller = Fresco.newDraweeControllerBuilder()
-                .setUri(mGif)
-                .setAutoPlayAnimations(true)
-                .build();
-        this.mPic4SDV.setController(controller);
+        // 动态图片处理
+        picHolding.gifHolding(mGif, mPic4SDV);
+        picHolding.gifHolding(mGif, mPic5SDV);
+        picHolding.gifHolding(mGif, mPic6SDV);
+
+        // 网格图片处理
+        picHolding.gridHolding(uri, Color.RED, mPic7SDV);
+        picHolding.gridHolding(uri, Color.BLACK, mPic8SDV);
+        picHolding.gridHolding(uri, Color.BLUE, mPic9SDV);
     }
 }
